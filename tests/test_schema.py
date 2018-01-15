06e6ac6fbb85cd84f46bcf265ac430e660e8d50e
Andreas Würl <andi@tryb.de>
2018-01-15 10:24:35 +0100

    add test for link parsing

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -12,3 +12,24 @@ class TestSchema(object):
 
         assert_that(result.data).is_empty()
         assert_that(result.schema.links).is_empty()
+
+    def test_creation_of_schema(self):
+
+        links_node = [
+            {
+                'rel': '<rel>',
+                'href': '<href>',
+                'method': '<method>',
+                'schema': '<schema>',
+                'targetSchema': '<targetSchema>'
+            }
+        ]
+
+        schema = Schema(links_node)
+
+        link = schema.show('<rel>')
+
+        assert_that(link.href).is_equal_to('<href>')
+        assert_that(link.method).is_equal_to('<method>')
+        assert_that(link.schema).is_equal_to('<schema>')
+        assert_that(link.target_schema).is_equal_to('<targetSchema>')
